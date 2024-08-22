5f7a73ad3e0a1bf6998074378e54f956fba8bc95
Victor Chien (MIS-簡翊倫) <10007212@MERRY.COM.TW>
2024-08-22 17:00:11 +0800

    2

--- goopimade.py
+++ goopimade.py
@@ -13,11 +13,10 @@ from selenium.webdriver.chrome.options import Options
 
 
 
-def go_to_product_and_add_to_cart(product_name):
-    start = time.time()
-    user_data_dir = 'C:\\Users\\10007212\\AppData\\Local\\Google\\Chrome\\User Data'
+def go_to_product_and_add_to_cart(product_name,product_type='type1',product_size='1號'):
+    
+    # user_data_dir = 'C:\\Users\\10007212\\AppData\\Local\\Google\\Chrome\\User Data'
     chrome_options = Options()
-    chrome_options.add_argument(f"user-data-dir={user_data_dir}")
     chrome_options.add_argument("--disable-animations")
     chrome_options.add_argument("--disable-background-networking")
     chrome_options.add_argument("--disable-backgrounding-occluded-windows")
@@ -60,48 +59,132 @@ def go_to_product_and_add_to_cart(product_name):
     chrome_options.add_experimental_option('detach', True)
 
     driver = webdriver.Chrome(options=chrome_options)  # 或使用其他瀏覽器
-    product_url = f"https://www.goopi.co/"
+    login_url = f"https://www.goopi.co/users/sign_in"
+    product_url = f"https://www.goopi.co/categories/goopimade-goopi-孤僻"
+    login_action="pass"
     
-    while True:
-        driver.get(product_url)
+    if  not login_action=="pass":
+        # 開啟登入網頁
+        driver.get(login_url)
+        print("請手動登入...")
+
+        # 等待用戶手動登入完成並確認登入成功
+        try:
+            WebDriverWait(driver, 300).until(EC.url_contains("https://www.goopi.co/?type=signin&platform=facebook"))
+            print("登入成功")
+        except Exception as e:
+            print("登入失敗或超時:", e)
+            driver.quit()
+            exit()
         
+    # 尋找商品
+    while True:
+
+        driver.get(product_url)
+        product_xpath=f"//a[contains(@class, 'quick-cart-item js-quick-cart-item') and .//div[contains(@class, 'title') and contains(text(), '{product_name}')]]/div[1]/div[1]/div"
         # 檢查商品是否存在，如果不存在則立即刷新
-        if not driver.find_elements(By.XPATH, f"//a[contains(@class, 'quick-cart-item js-quick-cart-item') and .//div[contains(@class, 'title') and contains(text(), '{product_name}')]]"):
+        try: 
+            product_element=driver.find_element(By.XPATH, product_xpath)
+            # product_element.click()
+            driver.execute_script("arguments[0].click();", product_element)
+            break
+        except:
             print("商品未出現，重新加載。")
             continue
-        else:
-        # 使用 WebDriverWait 和 expected_conditions 來等待元素出現
-            WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, f"//a[contains(@class, 'quick-cart-item js-quick-cart-item') and .//div[contains(@class, 'title') and contains(text(), '{product_name}')]]"))).click()
-            break
     
-    # 選擇尺寸，這裡假設我們選擇第一個尺寸
-    # 定位到 <select> 元素
-    select_element = driver.find_element(By.CLASS_NAME, "js-selectpicker")
-    select = Select(select_element)
-    # 選擇尺寸，這裡以選擇 "2號" 為例
-    select.select_by_visible_text("1號")
-    
-    # 加入購物車
-    # wait = WebDriverWait(driver, 10)  # 等待最多10秒
-    # add_to_cart_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="#btn-variable-buy-now"]')))
-    # add_to_cart_button.click()
-    buy_now_button = driver.find_element(By.CLASS_NAME, "btn-buy-now")
-    buy_now_button.click()
-    
-    # # 前往結帳頁面，這裡假設結帳頁面的按鈕ID為"checkout-button"
-    checkout_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "a.btn-checkout[data-e2e-id='checkout_button']"))
+    # 選擇購買類型
+    if product_type=='type1':
+
+        try:
+            select_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".selectpicker.js-selectpicker.form-control.form-control-sm.form-control-inline.ng-pristine.ng-untouched.ng-valid.ng-scope.ng-not-empty"))
+            )
+            # 使用Select類來與<select>元素互動
+            select = Select(select_element)
+
+            # 選擇尺寸，這裡以選擇 "1號" 為例
+            select.select_by_visible_text(product_size)
+        except:
+            print("無法選擇尺寸。")
+        try:
+            # 定位到"立即購買"按鈕
+            buy_now_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-buy-now.btn-purchase-action.btn-custom.ladda-button.form-control-inline.js-btn-main-buy-now.js-btn-show.js-btn-scroll"))
+            )
+            # 點擊"立即購買"按鈕
+            buy_now_button.click()
+        except:
+            print("無法點擊立即購買按鈕。") 
+
+
+
+    # 定位到"前往結帳"按鈕
+    checkout_button = WebDriverWait(driver, 20).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "a.btn.btn-success.btn-block.btn-checkout[data-e2e-id='checkout_button']"))
     )
+    # 點擊"前往結帳"按鈕
     checkout_button.click()
 
     # 在這裡可以繼續添加填寫結帳資訊的代碼
-    # 注意：自動填寫個人資訊和付款資訊涉及隱私和安全問題，請確保遵守相關法律法規和網站政策。
-    end = time.time()
-    print(f"執行時間：{end - start} 秒")
+
+    insert_html_and_click_script = """
+    // 找到目標<label>元素
+    var targetLabel = document.querySelector('label[for="order-delivery-location-code"]');
+
+    // 建立要新增的HTML內容
+    var newContent = `
+    <div class="margin-bottom-small">
+        <div class="row">
+        <span class="col-xs-12 col-sm-4">已選擇門市店號:</span>
+        <span class="col-xs-12 col-sm-8" name="order[delivery_data][location_code]" ng-non-bindable="" translate="no">187079</span>
+        </div>
+        <div class="row">
+        <span class="col-xs-12 col-sm-4">已選擇門市名稱:</span>
+        <span class="col-xs-12 col-sm-8" name="order[delivery_data][location_name]" ng-non-bindable="" translate="no">權美門市</span>
+        </div>
+        <div class="row">
+        <span class="col-xs-12 col-sm-4">門市地址:</span>
+        <span class="col-xs-12 col-sm-8" name="order[delivery_data][store_address]" ng-non-bindable="">台中市西區美村路一段736號</span>
+        </div>
+    </div>
+    `;
+
+    // 將新的HTML內容插入到目標<label>元素後面一格
+    targetLabel.insertAdjacentHTML('afterend', newContent);
+
+    // 找到 收件人資料與顧客資料相同
+    var targetCheckbox = document.querySelector('input[data-e2e-id="order-delivery-recipient-is-customer_checkbox"]');
+
+    // 模擬勾選目標<input>元素
+    targetCheckbox.click();
+
+    // 找到 我同意網站服務條款及隱私權政策
+    var targetButton = document.querySelector('input[data-e2e-id="checkout-policy_checkbox"]');
+
+    // 模擬點擊目標<input>元素
+targetButton.click();
+    """
+    #執行js填寫收件資訊
+    driver.execute_script(insert_html_and_click_script)
+    #送出訂單
+    checkout_script = """
+    // 找到 提交訂單
+    var targetButton = document.querySelector('button[data-e2e-id="place-order_button"]');
+
+    // 模擬點擊目標按鈕
+    targetButton.click();
+    """
+    #執行js提交訂單
+    # driver.execute_script(checkout_script)
+    
+
 
 # 使用範例
-# product='“br-m3g”-softbox-basic-pants-bone'
-product='T1-END.93 - “Nessie” Logo Tee'
-go_to_product_and_add_to_cart(product)
+
+product='“LM-S01” G-Lightweight Utility Shorts - L-Gray'
+# product='“BR-M3G” SOFTBOX Basic Pants - Shadow'
+product_type='type1' #type1:有size type2:無size(包包類)
+product_size='1號'
+go_to_product_and_add_to_cart(product,product_type,product_size)
 
 
